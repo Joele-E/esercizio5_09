7b96406e5a4df4b6616f664c78979f0429682dda
Joele-E <esposito.joelebiz@gmail.com>
2023-09-06 11:30:03 +0200

    before changes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
-import Prova from "./prova";
-import Cards from "./cards";
 import React from "react";
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.myCards = [
-      {
-        name: "card1",
-        img: "https://d9-wret.s3.us-west-2.amazonaws.com/assets/palladium/production/s3fs-public/thumbnails/image/md_rpd_sample_image_blu_vert.jpg",
-        qty: 3,
-      },
-      {
-        name: "card2",
-        img: "https://d9-wret.s3.us-west-2.amazonaws.com/assets/palladium/production/s3fs-public/thumbnails/image/md_rpd_sample_image_blu_vert.jpg",
-        qty: 2,
-      },
-      {
-        name: "card3",
-        img: "https://d9-wret.s3.us-west-2.amazonaws.com/assets/palladium/production/s3fs-public/thumbnails/image/md_rpd_sample_image_blu_vert.jpg",
-        qty: 1,
-      },
-    ];
-  }
-
   render() {
-    return <Cards cardsArray={this.myCards} />;
+    return <div></div>;
   }
 }
