54398ce4c218beeeb0025264942f11564acb431c
Joele-E <esposito.joelebiz@gmail.com>
2023-09-12 21:11:48 +0200

    exercise done

---
+++ src/Welcome.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class Welcome extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Welcome {this.props.name}</p>
+      </div>
+    );
+  }
+}
+
+Welcome.defaultProps = {
+  name: "DEFAULTUSER",
+};
