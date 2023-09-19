ced39ce71ceac2f8149fc23086aca29d03e442fb
Joele-E <esposito.joelebiz@gmail.com>
2023-09-19 22:41:52 +0200

    exercise done

--- src/Counter.js
+++ src/Counter.js
@@ -1,21 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export default class Counter extends React.Component {
-  state = {
-    counter: 0,
-  };
+export default function Counter() {
+  const [counter, setCounter] = useState(0);
 
-  componentDidMount() {
-    this.interval = setInterval(
-      () => this.setState({ counter: this.state.counter + 1 }),
-      1000
-    );
-  }
-  render() {
-    return (
-      <div>
-        <h1>The count is {this.state.counter}</h1>
-      </div>
-    );
-  }
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCounter(counter + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [counter]);
+
+  return (
+    <div>
+      <h1>The count is {counter}</h1>
+    </div>
+  );
 }
