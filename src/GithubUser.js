fc9c0579af7747c6943786f0af015b37e958206e
Joele-E <esposito.joelebiz@gmail.com>
2023-09-21 11:54:06 +0200

    fix: fixed exercise

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -2,33 +2,49 @@ import { useEffect, useState } from "react";
 
 export default function GithubUser({ username }) {
   const [userData, setUserData] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     let url = `https://api.github.com/users/${username}`;
     fetch(url)
-      .then((res) => res.json())
+      .then((res) => {
+        if (res.status !== 200) {
+          setError(new Error("Non Trovato"));
+        } else {
+          return res.json();
+        }
+      })
       .then((json) => {
         setUserData(json);
       })
-      .catch((err) => console.error(err));
+      .catch((err) => setError(err));
   }, [username]);
 
   return (
     <div>
-      <div
-        style={{
-          backgroundColor: "green",
-          display: "flex",
-          justifyContent: "space-between",
-          padding: "16px",
-          alignItems: "center",
-          gap: "30px",
-        }}
-      >
-        <h1>{userData.login}</h1>
-        <img src={userData.avatar_url} alt="" style={{ width: "50px" }} />
-      </div>
-      <p>Profilo creato: {userData.created_at}</p>
+      {error && <h1>Not Found</h1>}
+      {!error && (
+        <div>
+          <div
+            style={{
+              backgroundColor: "green",
+              display: "flex",
+              justifyContent: "space-between",
+              padding: "16px",
+              alignItems: "center",
+              gap: "30px",
+            }}
+          >
+            <h1>{userData && userData.login}</h1>
+            <img
+              src={userData && userData.avatar_url}
+              alt=""
+              style={{ width: "50px" }}
+            />
+          </div>
+          <p>Profilo creato: {userData && userData.created_at}</p>
+        </div>
+      )}
     </div>
   );
 }
