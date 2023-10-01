2380aa9ef38822bf907402e47b51200b004835e7
Joele-E <esposito.joelebiz@gmail.com>
2023-10-01 20:16:17 +0200

    exercise done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import Users from "./Users";
 import User from "./User";
 import Home from "./Home";
@@ -8,6 +8,15 @@ import ShowGithubUser from "./ShowGithubUser";
 const App = () => {
   return (
     <div>
+      <Link style={{}} to={"/"}>
+        Home
+      </Link>
+      <Link style={{ "margin-left": "20px" }} to={"users"}>
+        Users
+      </Link>
+      <Link style={{ "margin-left": "20px" }} to={"users/asdf"}>
+        Username
+      </Link>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="users" element={<Users />}>
--- src/Users.js
+++ src/Users.js
@@ -4,6 +4,7 @@ import { Link, Outlet } from "react-router-dom";
 const Users = () => {
   return (
     <div>
+      <h1>USERS</h1>
       <Outlet />
       <Link to={"/"}>HOME</Link>
     </div>
